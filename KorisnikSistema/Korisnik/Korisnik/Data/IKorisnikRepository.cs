3333acb5782579d15c41ff5d5a444ae86a0dd227
DusanMilasinovic <milasinovicdusan@gmail.com>
2022-01-25 14:36:00 +0100

    Internal memory korisnik crud operations

---
+++ KorisnikSistema/Korisnik/Korisnik/Data/IKorisnikRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Korisnik.Models;
+
+namespace Korisnik.Data
+{
+    public interface IKorisnikRepository
+    {
+        List<KorisnikModel> GetKorisniks(string ime = null);
+
+        KorisnikModel GetKorisniksById(int korisnikId);
+
+        KorisnikModel CreateKorisniks(KorisnikModel korisnikModel);
+
+        KorisnikModel UpdateKorisniks(KorisnikModel korisnikModel);
+
+        void DeleteKorisnik(int korisnikId);
+    }
+}
