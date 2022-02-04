d9c3a801c0574934825ff99704e317f91856ad18
dajanaIT36-2018 <73706235+dajanaIT36-2018@users.noreply.github.com>
2022-02-04 18:08:58 +0100

    Javno nadmetanje agregat- Komunikacija sa bazom podataka

--- JavnoNadmetanje/JavnoNadmetanjeAgregat/Data/StatusJavnogNadmetanjaRepository.cs
+++ JavnoNadmetanje/JavnoNadmetanjeAgregat/Data/StatusJavnogNadmetanjaRepository.cs
@@ -1,4 +1,5 @@
-﻿using JavnoNadmetanjeAgregat.Entities;
+﻿using AutoMapper;
+using JavnoNadmetanjeAgregat.Entities;
 
 using System;
 using System.Collections.Generic;
@@ -9,59 +10,46 @@ namespace JavnoNadmetanjeAgregat.Data
 {
     public class StatusJavnogNadmetanjaRepository : IStatusJavnogNadmetanjaRepository
     {
-        public static List<StatusJavnogNadmetanjaEntity> StatusiJavnogNadmetanja { get; set; } = new List<StatusJavnogNadmetanjaEntity>();
+        private readonly JavnoNadmetanjeContext context;
+        private readonly IMapper mapper;
 
-        public StatusJavnogNadmetanjaRepository()
+        public StatusJavnogNadmetanjaRepository(JavnoNadmetanjeContext context, IMapper mapper)
         {
-            FillData();
+
+            this.context = context;
+            this.mapper = mapper;
         }
 
-        private void FillData()
+        public bool SaveChanges()
         {
-            StatusiJavnogNadmetanja.AddRange(new List<StatusJavnogNadmetanjaEntity>
-            {
-                new StatusJavnogNadmetanjaEntity
-                {
-                    StatusJavnogNadmetanjaID = Guid.Parse("6a411c13-a195-48f7-8dbd-67596c3974c0"),
-                    NazivStatusaJavnogNadmetanja= "Status1"
-                  
-                },
-                new StatusJavnogNadmetanjaEntity
-                {
-                    StatusJavnogNadmetanjaID = Guid.Parse("1c7ea607-8ddb-493a-87fa-4bf5893e965b"),
-                    NazivStatusaJavnogNadmetanja="Status2"
-                }
-            });
+            return context.SaveChanges() > 0;
         }
         public StatusJavnogNadmetanjaEntity CreateStatusJavnogNadmetanja(StatusJavnogNadmetanjaEntity statusJavnogNadmetanja)
         {
             statusJavnogNadmetanja.StatusJavnogNadmetanjaID = Guid.NewGuid();
-            StatusiJavnogNadmetanja.Add(statusJavnogNadmetanja);
+            context.StatusiJavnihNadmetanja.Add(statusJavnogNadmetanja);
             StatusJavnogNadmetanjaEntity s = GetStatusJavnogNadmetanjaById(statusJavnogNadmetanja.StatusJavnogNadmetanjaID);
             return s;
         }
 
         public void DeleteStatusJavnogNadmetanja(Guid statusJavnogNadmetanjaID)
         {
-            StatusiJavnogNadmetanja.Remove(StatusiJavnogNadmetanja.FirstOrDefault(s => s.StatusJavnogNadmetanjaID == statusJavnogNadmetanjaID));
+            context.StatusiJavnihNadmetanja.Remove(context.StatusiJavnihNadmetanja.FirstOrDefault(s => s.StatusJavnogNadmetanjaID == statusJavnogNadmetanjaID));
         }
 
         public List<StatusJavnogNadmetanjaEntity> GetStatusJavnogNadmetanja()
         {
-            return (from s in StatusiJavnogNadmetanja select s).ToList();
+            return (from s in context.StatusiJavnihNadmetanja select s).ToList();
         }
 
         public StatusJavnogNadmetanjaEntity GetStatusJavnogNadmetanjaById(Guid statusJavnogNadmetanjaID)
         {
-            return StatusiJavnogNadmetanja.FirstOrDefault(s => s.StatusJavnogNadmetanjaID == statusJavnogNadmetanjaID);
+            return context.StatusiJavnihNadmetanja.FirstOrDefault(s => s.StatusJavnogNadmetanjaID == statusJavnogNadmetanjaID);
         }
 
         public StatusJavnogNadmetanjaEntity UpdateStatusJavnogNadmetanja(StatusJavnogNadmetanjaEntity statusJavnogNadmetanja)
         {
-            StatusJavnogNadmetanjaEntity s = GetStatusJavnogNadmetanjaById(statusJavnogNadmetanja.StatusJavnogNadmetanjaID);
-
-            s.NazivStatusaJavnogNadmetanja= statusJavnogNadmetanja.NazivStatusaJavnogNadmetanja;
-            return s;
+            throw new NotImplementedException();
         }
     }
 }
---
+++ JavnoNadmetanje/JavnoNadmetanjeAgregat/Data/TipJavnogNadmetanjaMockRepository.cs
@@ -0,0 +1,67 @@
+﻿using JavnoNadmetanjeAgregat.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JavnoNadmetanjeAgregat.Data
+{
+    public class TipJavnogNadmetanjaMockRepository : ITipJavnogNadmetanjaRepository
+    {
+        public static List<TipJavnogNadmetanjaEntity> TipoviJavnogNadmetanja { get; set; } = new List<TipJavnogNadmetanjaEntity>();
+
+        public TipJavnogNadmetanjaMockRepository()
+        {
+            FillData();
+        }
+
+        private void FillData()
+        {
+            TipoviJavnogNadmetanja.AddRange(new List<TipJavnogNadmetanjaEntity>
+            {
+                new TipJavnogNadmetanjaEntity
+                {
+                    TipJavnogNadmetanjaID = Guid.Parse("4D51C54C-4B90-46DE-8BB2-C8F74FB6FD9E"),
+                    NazivTipaJavnogNadmetanja= "Tip1"
+
+                },
+                new TipJavnogNadmetanjaEntity
+                {
+                    TipJavnogNadmetanjaID = Guid.Parse("0F173F98-C00A-4EB4-8131-AE00177371D8"),
+                    NazivTipaJavnogNadmetanja="Tip2"
+                }
+            });
+        }
+        public TipJavnogNadmetanjaEntity CreateTipJavnogNadmetanja(TipJavnogNadmetanjaEntity tipJavnogNadmetanja)
+        {
+            tipJavnogNadmetanja.TipJavnogNadmetanjaID = Guid.NewGuid();
+            TipoviJavnogNadmetanja.Add(tipJavnogNadmetanja);
+            TipJavnogNadmetanjaEntity t = GetTipJavnogNadmetanjaById(tipJavnogNadmetanja.TipJavnogNadmetanjaID);
+            return t;
+        }
+
+        public void DeleteTipJavnogNadmetanja(Guid tipJavnogNadmetanjaID)
+        {
+            TipoviJavnogNadmetanja.Remove(TipoviJavnogNadmetanja.FirstOrDefault(t => t.TipJavnogNadmetanjaID == tipJavnogNadmetanjaID));
+        }
+
+        public List<TipJavnogNadmetanjaEntity> GetTipJavnogNadmetanja()
+        {
+            return (from t in TipoviJavnogNadmetanja select t).ToList();
+        }
+
+        public TipJavnogNadmetanjaEntity GetTipJavnogNadmetanjaById(Guid tipJavnogNadmetanjaID)
+        {
+            return TipoviJavnogNadmetanja.FirstOrDefault(t => t.TipJavnogNadmetanjaID == tipJavnogNadmetanjaID);
+        }
+
+        public TipJavnogNadmetanjaEntity UpdateTipJavnogNadmetanja(TipJavnogNadmetanjaEntity tipJavnogNadmetanja)
+        {
+            TipJavnogNadmetanjaEntity t = GetTipJavnogNadmetanjaById(tipJavnogNadmetanja.TipJavnogNadmetanjaID);
+
+            t.NazivTipaJavnogNadmetanja = tipJavnogNadmetanja.NazivTipaJavnogNadmetanja;
+            return t;
+        }
+    }
+}
+
