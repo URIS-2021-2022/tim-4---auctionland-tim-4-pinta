4ffe350bb9065ba4f3a657308806bc668e64335b
Stefan Fink <stefan.fink123@gmail.com>
2022-02-19 16:01:30 +0100

    Povezivanje servisa 2

---
+++ Adresa/AdresaServis/Migrations/20220217154643_Create.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdresaServis.Migrations
+{
+    public partial class Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
