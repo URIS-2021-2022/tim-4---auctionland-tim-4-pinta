5fe803111a56413c898aa9a186ffe2b9fdbe125f
DusanMilasinovic <milasinovicdusan@gmail.com>
2022-02-20 21:17:46 +0100

    korisnik token

--- KorisnikSistema/Korisnik/Korisnik/Startup.cs
+++ KorisnikSistema/Korisnik/Korisnik/Startup.cs
@@ -1,16 +1,25 @@
 using Korisnik.Data;
+using Korisnik.Entities;
+using Korisnik.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Korisnik
@@ -23,13 +32,132 @@ namespace Korisnik
         }
 
         public IConfiguration Configuration { get; }
+        public object JwtBearerDefaults { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-            services.AddSingleton<IKorisnikRepository,KorisnikRepository>();
+            //setup.ReturnHttpNotAcceptable = true -> Vraæa status 406 (NotAcceptable) ukoliko klijent u Accept header-u zahteva traži neki format koji ne podržavamo (npr. application/xml)
+            services.AddControllers(setup =>
+            {
+                setup.ReturnHttpNotAcceptable = true;
+                //Ovde sa setup.Filters mozemo dodati response tipove za sve kontrolere
+                //setup.Filters.Add(new ProducesResponseTypeAttribute(StatusCodes.Status200OK));                
+            }
+            ).AddXmlDataContractSerializerFormatters() //Dodajemo podršku za XML tako da ukoliko klijent to traži u Accept header-u zahteva možemo da serializujemo payload u XML u odgovoru.
+            .ConfigureApiBehaviorOptions(setupAction => //Deo koji se odnosi na podržavanje Problem Details for HTTP APIs
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    //Kreiramo problem details objekat
+                    ProblemDetailsFactory problemDetailsFactory = context.HttpContext.RequestServices
+                        .GetRequiredService<ProblemDetailsFactory>();
+
+                    //Prosleðujemo trenutni kontekst i ModelState, ovo prevodi validacione greške iz ModelState-a u RFC format
+                    ValidationProblemDetails problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                        context.HttpContext,
+                        context.ModelState);
+
+                    //Ubacujemo dodatne podatke
+                    problemDetails.Detail = "Pogledajte polje errors za detalje.";
+                    problemDetails.Instance = context.HttpContext.Request.Path;
+
+                    //po defaultu se sve vraæa kao status 400 BadRequest, to je ok kada nisu u pitanju validacione greške,
+                    //ako jesu hoæemo da koristimo status 422 UnprocessibleEntity
+                    //tražimo info koji status kod da koristimo
+                    var actionExecutiongContext = context as ActionExecutingContext;
+
+                    //proveravamo da li postoji neka greška u ModelState-u, a takoðe proveravamo da li su svi prosleðeni parametri dobro parsirani
+                    //ako je sve ok parsirano ali postoje greške u validaciji hoæemo da vratimo status 422
+                    if ((context.ModelState.ErrorCount > 0) &&
+                        (actionExecutiongContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                    {
+                        problemDetails.Type = "https://google.com"; //inaèe treba da stoji link ka stranici sa detaljima greške
+                        problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                        problemDetails.Title = "Došlo je do greške prilikom validacije.";
+
+                        //sve vraæamo kao UnprocessibleEntity objekat
+                        return new UnprocessableEntityObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    };
+
+                    //ukoliko postoji nešto što nije moglo da se parsira hoæemo da vraæamo status 400 kao i do sada
+                    problemDetails.Status = StatusCodes.Status400BadRequest;
+                    problemDetails.Title = "Došlo je do greške prilikom parsiranja poslatog sadržaja.";
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+                };
+            });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            //Konfigurisanje Jwt autentifikacije za projekat
+            //Registrujemo Jwt autentifikacionu shemu i definisemo sve potrebne Jwt opcije
+
+            /*ne radi nzm koji paket
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });*/
+                        /*
+             * Izvor: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-5.0#lifetime-and-registration-options
+                - Transient objects are always different. The transient OperationId value is different in the IndexModel and in the middleware.
+                - Scoped objects are the same for each request but different across each request.
+                - Singleton objects are the same for every request.
+               Full link: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-5.0#overview-of-dependency-injection
+             */
+            //services.AddSingleton<IExamRegistrationRepository, ExamRegistrationMockRepository>(); //Koristimo mock repozitorijum
+            services.AddScoped<IKorisnikRepository, KorisnikRepository>(); //Koristimo konkretni repozitorijum
+            services.AddSingleton<IUserRepository, UserMockRepository>();
+            services.AddScoped<IAuthenticationHelper, AuthenticationHelper>();
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc("KorisnikOpenApiSpecification",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Korisnik API",
+                        Version = "1",
+                       
+                        Description = ".",
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                        {
+                            Name = "Marko Markoviæ",
+                            Email = "marko@mail.com",
+                            Url = new Uri("http://www.ftn.uns.ac.rs/")
+                        },
+                        License = new Microsoft.OpenApi.Models.OpenApiLicense
+                        {
+                            Name = "FTN licence",
+                            Url = new Uri("http://www.ftn.uns.ac.rs/")
+                        },
+                        TermsOfService = new Uri("http://www.ftn.uns.ac.rs/examRegistrationTermsOfService")
+                    });
+
+                //Pomocu refleksije dobijamo ime XML fajla sa komentarima (ovako smo ga nazvali u Project -> Properties)
+                var xmlComments = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
+
+                //Pravimo putanju do XML fajla sa komentarima
+                var xmlCommentsPath = Path.Combine(AppContext.BaseDirectory, xmlComments);
+
+                //Govorimo swagger-u gde se nalazi dati xml fajl sa komentarima
+                setupAction.IncludeXmlComments(xmlCommentsPath);
+            });
+
+            //Dodajemo DbContext koji želimo da koristimo
+            services.AddDbContext<KorisnikContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +168,28 @@ namespace Korisnik
                 app.UseDeveloperExceptionPage();
                 
             }
+            else //Ukoliko se nalazimo u Production modu postavljamo default poruku za greške koje nastaju na servisu
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("Došlo je do neoèekivane greške. Molimo pokušajte kasnije.");
+                    });
+                });
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(setupAction =>
+            {
+                //Podesavamo endpoint gde Swagger UI moze da pronadje OpenAPI specifikaciju
+                setupAction.SwaggerEndpoint("/swagger/KorisnikOpenApiSpecification/swagger.json", "Korisnik API");
+                setupAction.RoutePrefix = "swagger"; //Dokumentacija ce sada biti dostupna na root-u (ne mora da se pise /swagger)
+            });
+
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
 
