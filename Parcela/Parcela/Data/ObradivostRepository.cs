c589c2b7983f425bd076a0f5ee69d74fe4b53038
stefanradulovicuns <stefanradulovic@uns.ac.rs>
2022-02-22 16:40:41 +0100

    Refaktorisanje 1

--- Parcela/Parcela/Data/ObradivostRepository.cs
+++ Parcela/Parcela/Data/ObradivostRepository.cs
@@ -45,7 +45,8 @@ namespace Parcela.Data
 
         public void UpdateObradivost(ObradivostEntity obradivost)
         {
-            
+            //Nije potrebna implementacija jer EF core prati entitet koji smo izvukli iz baze
+            //i kada promenimo taj objekat i odradimo SaveChanges sve izmene će biti perzistirane
         }
     }
 }
